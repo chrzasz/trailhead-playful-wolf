1d9df8cf6632b24ee32ab2b49b75f29439ed0f49
Grzegorz Chrząszczyk <chrzaszczyk@gmail.com>
2021-04-02 02:09:44 +0200

    LWC bikeCard

--- force-app/main/default/lwc/bikeCard/bikeCard.js
+++ force-app/main/default/lwc/bikeCard/bikeCard.js
@@ -1,10 +1,31 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { getSObjectValue } from '@salesforce/apex';
+import getSingleContact from '@salesforce/apex/ContactController.getSingleContact';
+import NAME_FIELD from '@salesforce/schema/Contact.Name';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import PHONE_FIELD from '@salesforce/schema/Contact.MobilePhone';
+
 
 export default class BikeCard extends LightningElement {
-   name = 'Electra X4';
+
    description = 'A sweet bike built for comfort.';
    category = 'Mountain';
    material = 'Steel';
    price = '$2,700';
    pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
- }
+
+   @wire(getSingleContact) contact;
+
+   get name() {
+      return this.contact.data ? getSObjectValue(this.contact.data, NAME_FIELD) : '';
+   }
+   get email() {
+      return this.contact.data ? getSObjectValue(this.contact.data, EMAIL_FIELD) : '';
+   }
+   get phone() {
+      return this.contact.data ? getSObjectValue(this.contact.data, PHONE_FIELD) : '';
+   }
+
+
+
+}
